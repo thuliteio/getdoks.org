a8259737576c4f67ca19207c72f8001baf05711e
Henk Verlinde <henk@ventizo.com>
2021-03-02 16:10:40 +0100

    test: update submission-created

--- assets/lambda/submission-created.js
+++ assets/lambda/submission-created.js
@@ -17,6 +17,7 @@ exports.handler = async event => {
     },
     body: JSON.stringify({
       email: payload.email,
+      referrer_url: payload.page,
       utm_source: 'website',
       utm_medium: 'subscribe-form'
     }),
