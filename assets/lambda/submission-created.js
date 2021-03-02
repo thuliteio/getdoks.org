fd3ea603e8bb174580d7eb47b92a30b31e5f41e8
Henk Verlinde <henk@ventizo.com>
2021-03-02 15:36:55 +0100

    test: update submission-created

--- assets/lambda/submission-created.js
+++ assets/lambda/submission-created.js
@@ -4,10 +4,11 @@ const fetch = require('node-fetch').default
 const { EMAIL_TOKEN } = process.env
 
 exports.handler = async event => {
+
   const payload = JSON.parse(event.body).payload
   console.log(`Recieved a submission: ${payload.email}`)
 
-  return await fetch('https://api.buttondown.email/v1/subscribers', {
+  const rawResponse = await fetch('https://api.buttondown.email/v1/subscribers', {
     method: 'post',
     headers: {
       Authorization: `Token ${EMAIL_TOKEN}`,
@@ -16,9 +17,10 @@ exports.handler = async event => {
     },
     body: JSON.stringify({ "email": payload.email, "referrer_url": payload.page }),
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
-    })
     .catch(error => ({ statusCode: 422, body: String(error) }))
+
+    const content = await rawResponse.json();
+
+    console.log(content);
+
 }
