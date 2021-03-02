bea0fb1aaa05f7444e9ccc263cc20211f41fa92f
Henk Verlinde <henk@ventizo.com>
2021-03-02 15:30:07 +0100

    test: update submission-created

--- assets/lambda/submission-created.js
+++ assets/lambda/submission-created.js
@@ -7,17 +7,18 @@ exports.handler = async event => {
   const payload = JSON.parse(event.body).payload
   console.log(`Recieved a submission: ${payload.email}`)
 
-  return fetch('https://api.buttondown.email/v1/subscribers', {
-    method: 'POST',
+  return await fetch('https://api.buttondown.email/v1/subscribers', {
+    method: 'post',
     headers: {
       Authorization: `Token ${EMAIL_TOKEN}`,
+      'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ "email": payload.email, "referrer_url": payload.page }),
   })
-    .then(response => response.json())
-    .then(response => {
-      console.log(`Submitted to Buttondown:\n ${response}`)
+    .then(response => await response.json())
+    .then(data => {
+      console.log(`Submitted to Buttondown:\n ${data}`)
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
 }
