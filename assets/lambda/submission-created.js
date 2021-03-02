0fb75f5922e14ccb51a9998f1aa60c65202d4124
Henk Verlinde <henk@ventizo.com>
2021-03-02 12:48:13 +0100

    fix: update script submission-created

--- assets/lambda/submission-created.js
+++ assets/lambda/submission-created.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
   require('dot-env').config()
-  const fetch = require('node-fetch')
+  const fetch = require('node-fetch').default
   const { EMAIL_TOKEN } = process.env
   exports.handler = async event => {
     const email = JSON.parse(event.body).payload.email
