fb93d5a8ea321f011b69c46df1a394dbd97fa03f
Henk Verlinde <henk@ventizo.com>
2021-03-02 14:49:34 +0100

    test: update submission-created

--- assets/lambda/submission-created.js
+++ assets/lambda/submission-created.js
@@ -1,19 +1,19 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const fetch = require("node-fetch").default
+const fetch = require('node-fetch').default
 const { EMAIL_TOKEN } = process.env
 
 exports.handler = async event => {
   const payload = JSON.parse(event.body).payload
   console.log(`Recieved a submission: ${payload.email}`)
 
-  return fetch("https://api.buttondown.email/v1/subscribers", {
-    method: "POST",
+  return fetch('https://api.buttondown.email/v1/subscribers', {
+    method: 'POST',
     headers: {
       Authorization: `Token ${EMAIL_TOKEN}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ "email": payload.email, "notes": payload.page }),
+    body: JSON.stringify({ "email": payload.email, "referrer_url": payload.page }),
   })
     .then(response => response.json())
     .then(data => {
