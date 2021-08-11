53c6fddec2f05c1ab1c96f3ccb5ea187cc26fb42
Henk Verlinde <henk@ventizo.com>
2021-08-11 19:47:47 +0200

    feat: add carbonads

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -20,11 +20,12 @@ module.exports = {
         'td',
         'h5',
         ...whitelister([
+          './assets/scss/components/_carbonads.scss',
           './assets/scss/components/_buttons.scss',
           './assets/scss/components/_code.scss',
           './assets/scss/components/_syntax.scss',
           './assets/scss/components/_search.scss',
-          './assets/scss/common/_ads.scss',
+          // './assets/scss/common/_ads.scss',
           './assets/scss/common/_dark.scss',
         ]),
       ],
