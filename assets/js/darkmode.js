9689c75a66477a0fd4a75d050cf4e87f0eb0b75d
Henk Verlinde <henk@ventizo.com>
2021-09-01 16:19:53 +0200

    feat: update dark mode logic

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,14 +1,38 @@
-if (document.getElementById('mode') !== null) {
-  document.getElementById('mode').addEventListener('click', () => {
+const mode = document.getElementById('mode');
 
-    document.body.classList.toggle('dark');
-    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+if (mode !== null) {
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+
+    if (event.matches) {
+
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-dark-mode', '');
+
+    } else {
+
+      localStorage.setItem('theme', 'light');
+      document.documentElement.removeAttribute('data-dark-mode');
+
+    }
+
+  })
+
+  mode.addEventListener('click', () => {
+
+    document.documentElement.toggleAttribute('data-dark-mode');
+    localStorage.setItem('theme', document.documentElement.hasAttribute('data-dark-mode') ? 'dark' : 'light');
 
   });
 
   if (localStorage.getItem('theme') === 'dark') {
 
-    document.body.classList.add('dark');
+    document.documentElement.setAttribute('data-dark-mode', '');
+
+  } else {
+
+    document.documentElement.removeAttribute('data-dark-mode');
 
   }
+
 }
