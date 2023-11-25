c392e6b4fc6c09aabe1d580342a5c0165e41bd18
Henk Verlinde <henk@henkverlinde.com>
2023-11-25 11:57:50 +0100

    config: update Plausible script tag

--- assets/js/plausible.js
+++ assets/js/plausible.js
@@ -6,3 +6,22 @@ const plausibleDomain = params.plausibleDomain;
 const { trackPageview, trackEvent } = Plausible({
   domain: plausibleDomain,
 })
+
+trackPageview()
+
+/*
+if (document.querySelector('#subscribeButton') !== null) {
+  document.getElementById('subscribeButton').addEventListener('click', function(){
+    trackEvent('Signup');
+  });
+}
+*/
+
+if (document.querySelector('#subscribeButton') !== null) {
+  const button = document.getElementById('subscribeButton');
+  const page = button.getAttribute('data-title');
+
+  button.addEventListener('click', function(){
+    trackEvent('Signup', { props: { Page: `${page}`} });
+  });
+}
