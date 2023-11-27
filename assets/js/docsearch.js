12d32e0e5b38da95a0eef4137f70954d745cc58a
Henk Verlinde <henk@henkverlinde.com>
2023-11-27 12:29:04 +0100

    feat: add DocSearch

---
+++ assets/js/docsearch.js
@@ -0,0 +1,16 @@
+import docsearch from '@docsearch/js';
+
+docsearch({
+  container: '#docsearch',
+  appId: 'KMWY81ZWS3',
+  indexName: 'getdoks',
+  apiKey: '691951526b11e1a708258e4e1a51adc7',
+  insights: true,
+});
+
+const onClick = function() {
+  document.getElementsByClassName("DocSearch-Button")[0].click();  
+}
+
+document.getElementById('searchToggleMobile').onclick = onClick;
+document.getElementById('searchToggleDesktop').onclick = onClick;
