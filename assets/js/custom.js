837f76dc3059a36139e04787c4db5a9227de353d
Henk Verlinde <henk@henkverlinde.com>
2025-08-03 20:19:14 +0200

    Update UX On this page navigation mobile screens

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,3 +43,17 @@ if (copyMarkdownBtn) {
       });
   });
 }
+
+// Close mobile TOC details when clicking on a link
+document.addEventListener('click', function (e) {
+  // Check if the clicked element is a link within the mobile TOC
+  const tocMobile = e.target.closest('.toc-mobile');
+  if (tocMobile && e.target.tagName === 'A') {
+    // Find the details element within the mobile TOC
+    const details = tocMobile.querySelector('details');
+    if (details && details.open) {
+      // Close the details element
+      details.open = false;
+    }
+  }
+});
