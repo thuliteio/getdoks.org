1ba047a238535db6ca769ec5a7f2c83e121c22f8
Henk Verlinde <henk@henkverlinde.com>
2025-08-11 21:03:11 +0200

    Add basicLightbox classes to safelist purgeCSSPlugin

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -64,27 +64,27 @@ import * as basicLightbox from 'basiclightbox'
 const images = document.querySelectorAll('.lightbox img, img.lightbox')
 
 images.forEach(img => {
-	const html = img.outerHTML
+  const html = img.outerHTML
 
-	// Function to handle escape key
-	const handleEscapeKey = (event) => {
-		if (event.key === 'Escape') {
-			instance.close()
-		}
-	}
+  // Function to handle escape key
+  const handleEscapeKey = (event) => {
+    if (event.key === 'Escape') {
+      instance.close()
+    }
+  }
 
-	const instance = basicLightbox.create(html, {
-		onShow: () => {
-			// Add escape key listener when lightbox is shown
-			document.addEventListener('keydown', handleEscapeKey)
-		},
-		onClose: () => {
-			// Remove escape key listener when lightbox is closed
-			document.removeEventListener('keydown', handleEscapeKey)
-		}
-	})
+  const instance = basicLightbox.create(html, {
+    onShow: () => {
+      // Add escape key listener when lightbox is shown
+      document.addEventListener('keydown', handleEscapeKey)
+    },
+    onClose: () => {
+      // Remove escape key listener when lightbox is closed
+      document.removeEventListener('keydown', handleEscapeKey)
+    }
+  })
 
-	img.onclick = () => {
-		instance.show()
-	}
+  img.onclick = () => {
+    instance.show()
+  }
 })
